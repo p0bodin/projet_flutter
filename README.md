b0c3bdd534e8c6f3fc20dbd746db7a60392b2740
p0bodin <p0bodin@enib.fr>
2026-01-12 15:47:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_ihm
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_ihm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_ihm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
